f9a664a65f3399b790fad99b840bdce08c254ce2
SINGHAMIT-7989 <220010004@iitdh.ac.in>
2024-07-11 16:17:33 +0530

    Checkout and Product Detail Page Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import ProductList from './features/product-list/ProductList';
+import ProductList from './features/product-list/components/ProductList';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
 
 import * as React from "react";
 import { createRoot } from "react-dom/client";
@@ -14,7 +15,7 @@ import {
   Route,
   Link,
 } from "react-router-dom";
-
+import ProductDetailPage from './pages/ProductDetailPage'
 const router = createBrowserRouter([
   {
     path: "/",
@@ -34,6 +35,15 @@ const router = createBrowserRouter([
     element: <CartPage></CartPage>,
   },
 
+  {
+    path: "/checkout",
+    element: <Checkout></Checkout>,
+  },
+
+  {
+    path: "/product-detail",
+    element: <ProductDetailPage></ProductDetailPage>,
+  },
 
 
 ]);
